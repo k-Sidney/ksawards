12130ecb387114f18a013de209502117bbbc322c
k-Sidney <kleubersidney@hotmail.com>
2024-04-08 16:50:03 -0300

    Project Created

---
+++ backend/src/main/java/com/kscorp/ksawards/KsawardsApplication.java
@@ -0,0 +1,13 @@
+package com.kscorp.ksawards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsawardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsawardsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/kscorp/ksawards/KsawardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kscorp.ksawards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsawardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

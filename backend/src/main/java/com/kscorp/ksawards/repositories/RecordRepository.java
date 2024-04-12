9fd66e309001d39903af82612e30f8a24b8f6a5d
k-Sidney <kleubersidney@hotmail.com>
2024-04-12 16:47:19 -0300

    records endpoints and ready for productions

--- backend/src/main/java/com/kscorp/ksawards/repositories/RecordRepository.java
+++ backend/src/main/java/com/kscorp/ksawards/repositories/RecordRepository.java
@@ -1,6 +1,11 @@
 package com.kscorp.ksawards.repositories;
 
+import java.time.Instant;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.kscorp.ksawards.entities.Record;
@@ -8,4 +13,8 @@ import com.kscorp.ksawards.entities.Record;
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
 
+	@Query("SELECT obj FROM Record obj WHERE"
+			+ "(coalesce(:min, null) IS NULL OR obj.moment >= :min) AND "
+			+ "(coalesce(:max, null) IS NULL OR obj.moment <= :max)")
+	Page<Record> findByMoments(Instant min, Instant max, PageRequest pageRequest);
 }
--- backend/src/main/java/com/kscorp/ksawards/services/RecordService.java
+++ backend/src/main/java/com/kscorp/ksawards/services/RecordService.java
@@ -3,6 +3,8 @@ package com.kscorp.ksawards.services;
 import java.time.Instant;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,27 +15,31 @@ import com.kscorp.ksawards.entities.Record;
 import com.kscorp.ksawards.repositories.GameRepository;
 import com.kscorp.ksawards.repositories.RecordRepository;
 
-
 @Service
 public class RecordService {
 
 	@Autowired
 	private RecordRepository repository;
-	
-	@Autowired 
+
+	@Autowired
 	private GameRepository gameRepository;
-	
+
 	@Transactional
-	public RecordDTO insert (RecordInsertDTO dto) {
+	public RecordDTO insert(RecordInsertDTO dto) {
 		Record entity = new Record();
 		entity.setName(dto.getName());
 		entity.setAge(dto.getAge());
 		entity.setMoment(Instant.now());
-		
+
 		Game game = gameRepository.getOne(dto.getGameId());
 		entity.setGame(game);
-		
+
 		entity = repository.save(entity);
 		return new RecordDTO(entity);
+	}
+
+	@Transactional(readOnly = true)
+	public Page<RecordDTO> findByMoments(Instant minDate, Instant maxDate, PageRequest pageRequest) {
+		return repository.findByMoments(minDate, maxDate, pageRequest).map(x -> new RecordDTO(x));
 	}
 }
